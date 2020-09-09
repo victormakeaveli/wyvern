6b2e7bc63dd3b094a047b8399b28027492411771
Victor <victor.makeaveli@gmail.com>
2020-09-08 22:39:13 -0300

    changes

--- wyvern/models.py
+++ wyvern/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class People(models.Model):
+    name = models.CharField(max_length=12)
+    role = models.CharField(max_length=11)
+    date_created = models.DateTimeField('date created')
+    
+    def __str__(self):
+        return self.name
+    
--- wyvern/views.py
+++ wyvern/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
-
+from django.http import HttpResponse
+from .models import People
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+def Detail(request, people_pk):
+    return HttpResponse 
