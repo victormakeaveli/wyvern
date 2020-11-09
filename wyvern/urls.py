fe1eab3e6f7e1e5a221297737bbcc40efe588939
Victor <victor.makeaveli@gmail.com>
2020-11-09 15:41:34 -0300

    lts

--- wyvern/urls.py
+++ wyvern/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
         path('', views.HomePageView.as_view(), name='home'),
         path('clients/', views.ClientPageView.as_view(), name='clients'),
--- wyvern/views.py
+++ wyvern/views.py
@@ -1,38 +1,24 @@
-from django.views.generic import TemplateView, ListView, DetailView
-from django.template import loader
+from django.views.generic import TemplateView, ListView
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views import generic
 
 from .models import Client, Counter
 
-class HomePageView(generic.ListView):
+class HomePageView(TemplateView):
     template_name = 'index.html'
-    context_object_name = 'main_page'
+
 
 class ClientPageView(ListView):
     template_name = 'clients.html'
-    
-    def get_queryset(self):
-        """
-        im trying to show a counter
-        """
-            
-        counter = Counter.objects.last()
-        if not counter:
-            counter = Counter.objects.create()
-        
-        counter.count =+ 1
-        counter.save()
+    model = Client
 
-        return counter
-    
+
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
 
 def client_detail(request, client_id):
     client = Client.objects.get(id=client_id)
     context = { "client_var": client }
 
     return render(request, "detail.html", context)
-
